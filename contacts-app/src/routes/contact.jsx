82903ab9c1d4798c3d7a3f3087def1f2a1839c58
abdulwaheed-apk <ab.waheedkashmiri@gmail.com>
2023-03-10 23:08:11 +0500

    react-router-dom practice contacts-app

--- contacts-app/src/routes/contact.jsx
+++ contacts-app/src/routes/contact.jsx
@@ -1,14 +1,28 @@
-import { Form, useLoaderData } from "react-router-dom"
-import { getContact } from "../contacts"
+import { Form, useLoaderData, useFetcher } from 'react-router-dom'
+import { getContact, updateContact } from '../contacts'
+
+export async function action({ request, params }) {
+  let formData = await request.formData()
+  return updateContact(params.contactId, {
+    favorite: formData.get('favorite') === 'true',
+  })
+}
 
 export async function loader({ params }) {
-  return getContact(params.contactId)
+ const contact = await getContact(params.contactId)
+ if (!contact) {
+   throw new Response('', {
+     status: 404,
+     statusText: 'Not Found',
+   })
+ }
+ return { contact }
 }
 export default function Contact() {
-  const contact = useLoaderData()
+  const { contact } = useLoaderData()
 
   return (
-    <div id="contact">
+    <div id='contact'>
       <div>
         <img key={contact.avatar} src={contact.avatar || null} />
       </div>
@@ -21,13 +35,13 @@ export default function Contact() {
             </>
           ) : (
             <i>No Name</i>
-          )}{" "}
+          )}{' '}
           <Favorite contact={contact} />
         </h1>
 
         {contact.twitter && (
           <p>
-            <a target="_blank" href={`https://twitter.com/${contact.twitter}`}>
+            <a target='_blank' href={`https://twitter.com/${contact.twitter}`}>
               {contact.twitter}
             </a>
           </p>
@@ -36,19 +50,19 @@ export default function Contact() {
         {contact.notes && <p>{contact.notes}</p>}
 
         <div>
-          <Form action="edit">
-            <button type="submit">Edit</button>
+          <Form action='edit'>
+            <button type='submit'>Edit</button>
           </Form>
           <Form
-            method="post"
-            action="destroy"
+            method='post'
+            action='destroy'
             onSubmit={(event) => {
-              if (!confirm("Please confirm you want to delete this record.")) {
+              if (!confirm('Please confirm you want to delete this record.')) {
                 event.preventDefault()
               }
             }}
           >
-            <button type="submit">Delete</button>
+            <button type='submit'>Delete</button>
           </Form>
         </div>
       </div>
@@ -58,16 +72,20 @@ export default function Contact() {
 
 function Favorite({ contact }) {
   // yes, this is a `let` for later
+  const fetcher = useFetcher()
   let favorite = contact.favorite
+ if (fetcher.formData) {
+   favorite = fetcher.formData.get('favorite') === 'true'
+ }
   return (
-    <Form method="post">
+    <fetcher.Form method='post'>
       <button
-        name="favorite"
-        value={favorite ? "false" : "true"}
-        aria-label={favorite ? "Remove from favorites" : "Add to favorites"}
+        name='favorite'
+        value={favorite ? 'false' : 'true'}
+        aria-label={favorite ? 'Remove from favorites' : 'Add to favorites'}
       >
-        {favorite ? "★" : "☆"}
+        {favorite ? '★' : '☆'}
       </button>
-    </Form>
+    </fetcher.Form>
   )
 }
--- contacts-app/src/routes/root.jsx
+++ contacts-app/src/routes/root.jsx
@@ -1,43 +1,62 @@
+import { useEffect } from 'react'
 import {
-  Link,
   Outlet,
+  Link,
   useLoaderData,
   Form,
   redirect,
   NavLink,
-} from "react-router-dom"
-import { getContacts, createContact } from "../contacts"
+  useNavigation,
+  useSubmit,
+} from 'react-router-dom'
+import { getContacts, createContact } from '../contacts'
 
 export async function action() {
   const contact = await createContact()
   return redirect(`/contacts/${contact.id}/edit`)
 }
-
-export async function loader() {
-  const contacts = await getContacts()
-  return { contacts }
+export async function loader({ request }) {
+  const url = new URL(request.url)
+  const q = url.searchParams.get('q')
+  const contacts = await getContacts(q)
+  return { contacts, q }
 }
-
 export default function Root() {
-  const { contacts } = useLoaderData()
+  const { contacts, q } = useLoaderData()
+  const navigation = useNavigation()
+  const submit = useSubmit()
+  const searching =
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has('q')
+  useEffect(() => {
+    document.getElementById('q').value = q
+  }, [q])
   return (
     <>
-      <div id="sidebar">
+      <div id='sidebar'>
         <h1>React Router Contacts</h1>
         <div>
-          <form id="search-form" role="search">
+          <Form id='search-form' role='search'>
             <input
-              id="q"
-              aria-label="Search contacts"
-              placeholder="Search"
-              type="search"
-              name="q"
+              id='q'
+              className={searching ? 'loading' : ''}
+              aria-label='Search contacts'
+              placeholder='Search'
+              type='search'
+              name='q'
+              defaultValue={q}
+              onChange={(event) => {
+                const isFirstSearch = q == null
+                submit(event.currentTarget.form, {
+                  replace: !isFirstSearch,
+                })
+              }}
             />
-            <div id="search-spinner" aria-hidden hidden={true} />
-            <div className="sr-only" aria-live="polite"></div>
-          </form>
-          <Form method="post">
-            <button type="submit">New</button>
+            <div id='search-spinner' aria-hidden hidden={!searching} />
+            <div className='sr-only' aria-live='polite'></div>
+          </Form>
+          <Form method='post'>
+            <button type='submit'>New</button>
           </Form>
         </div>
         <nav>
@@ -46,21 +65,19 @@ export default function Root() {
               {contacts.map((contact) => (
                 <li key={contact.id}>
                   <NavLink
-                    to={`/contacts/${contact.id}`}
+                    to={`contacts/${contact.id}`}
                     className={({ isActive, isPending }) =>
-                      isActive ? "active" : isPending ? "pending" : ""
+                      isActive ? 'active' : isPending ? 'pending' : ''
                     }
                   >
-                    <Link to={`contacts/${contact.id}`}>
-                      {contact.first || contact.last ? (
-                        <>
-                          {contact.first} {contact.last}
-                        </>
-                      ) : (
-                        <i>No Name</i>
-                      )}{" "}
-                      {contact.favorite && <span>★</span>}
-                    </Link>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{' '}
+                    {contact.favorite && <span>★</span>}
                   </NavLink>
                 </li>
               ))}
@@ -72,7 +89,10 @@ export default function Root() {
           )}
         </nav>
       </div>
-      <div id="detail">
+      <div
+        id='detail'
+        className={navigation.state === 'loading' ? 'loading' : ''}
+      >
         <Outlet />
       </div>
     </>
